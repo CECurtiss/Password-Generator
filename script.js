fcc62810f47451a2e8388185a0d5da908252e71b
CECurtiss <curtissclinton@gmail.com>
2022-09-28 23:34:05 -0500

    deleted copy of js file and added arrays

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
+var lowercaseArray = ("a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z")
+var uppercaseArray = ("A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z")
+var numberArray = ("0,1,2,3,4,5,6,7,8,9")
+var specialCharacterArray = ""
+
 function passwordInit(){
-window.prompt("Please choose the number of characters (between 8-128) your password will be:");
+  var charNum = prompt("Please choose the number of characters (between 8-128) your password will be:");
+ 
+ if (charNum <= 7) {
+ alert("The password length must be between 8 and 128 characters.");
+ } else if (charNum >= 129) {
+ alert("The password length must be between 8 and 128 characters.");
+ } else (charNum === true);
 
+ var lowercaseConfirm = confirm("Would you like your password to contain lower case letters?");
+
+ if (lowercaseConfirm = false) {
+  
+ }
 }
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+ 
+ // Get references to the #generate element
+ // var generateBtn = document.querySelector("#generate");
+ 
+ // // Write password to the #password input
+ // function writePassword() {
+ //   var password = generatePassword();
+ //   var passwordText = document.querySelector("#password");
+ 
+ //   passwordText.value = password;
+ 
+ // }
+ 
+ // // Add event listener to generate button
+ // generateBtn.addEventListener("click", writePassword);
+ 
