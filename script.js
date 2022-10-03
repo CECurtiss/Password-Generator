1abc23cafc6c6a63c2b9ccd2777316ccc3fda3bb
CECurtiss <curtissclinton@gmail.com>
2022-10-03 12:35:31 -0500

    added characaters to array and added conditions

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var lowercaseArray = ("a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z")
 var uppercaseArray = ("A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z")
 var numberArray = ("0,1,2,3,4,5,6,7,8,9")
-var specialCharacterArray = ""
+var specialCharacterArray = ("`~!@#$%^&*()")
+
+
 
 function passwordInit(){
   var charNum = prompt("Please choose the number of characters (between 8-128) your password will be:");
@@ -14,11 +16,26 @@ function passwordInit(){
 
  var lowercaseConfirm = confirm("Would you like your password to contain lower case letters?");
 
- if (lowercaseConfirm = false) {
-  
- }
-}
+ var uppercaseConfirm = confirm("Would you like your password to contain upper case letters?");
 
+ var numberConfirm = confirm("Would you like your password to contain numbers?");
+
+ var specialCharConfirm = confirm("Would you like your password to contain special characters?");
+
+if 
+  (lowercaseConfirm === true && 
+  uppercaseConfirm === true && 
+  numberConfirm === true && 
+  specialCharConfirm === true) {
+  function generatePassword()
+  
+  // } else if 
+  // (lowercaseConfirm===true &&
+  //  uppercaseConfirm ===true &&)
+
+
+}
+}
 
  
  // Get references to the #generate element
